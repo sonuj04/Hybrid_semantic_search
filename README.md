dd38f9d6d488239e11d56b9efb1bcc5fa9468fe8
Sonu <sonujhajharia721@gmail.com>
2026-02-09 01:15:59 +0530

    Implement basic semantic search pipeline

---
+++ README.md
@@ -0,0 +1,19 @@
+# Semantic Search Engine (Elasticsearch + NLP)
+
+A basic semantic search engine built using:
+- Python
+- Elasticsearch
+- Sentence Transformers
+- Pandas
+
+## Features
+- Indexes product catalog data
+- Performs semantic search over product descriptions
+- Returns relevant products based on meaning, not keywords
+
+## Setup Instructions
+
+### 1. Clone the repository
+```bash
+git clone <https://github.com/sonuj04/Private-Proj.git>
+cd semantic_search
---
+++ searchApp.py
@@ -0,0 +1,80 @@
+import streamlit as st
+from elasticsearch import Elasticsearch
+from sentence_transformers import SentenceTransformer
+
+indexName = "all_products"
+
+try:
+    from dotenv import load_dotenv
+    import os
+
+    load_dotenv()  # reads .env into environment
+
+    es = Elasticsearch(
+        os.environ["ES_URL"],
+        basic_auth=(
+            os.environ["ES_USERNAME"],
+            os.environ["ES_PASSWORD"]
+        ),
+        ca_certs=os.environ["ES_CA_CERT"]
+    )
+except ConnectionError as e:
+    print("Connection Error:", e)
+    
+if es.ping():
+    print("Successfully connected to ElasticSearch!!")
+else:
+    print("Oops!! Can not connect to Elasticsearch!")
+
+
+
+
+def search(input_keyword):
+    model = SentenceTransformer('all-mpnet-base-v2')
+    vector_of_input_keyword = model.encode(input_keyword)
+
+    res = es.search(
+        index="all_products",
+        knn={
+            "field": "DescriptionVector",
+            "query_vector": vector_of_input_keyword,
+            "k": 2,
+            "num_candidates": 500
+        },
+    _source=["ProductName", "Description"]
+    )
+    results = res["hits"]["hits"]
+
+    return results
+
+def main():
+    st.title("Search Fashion Products")
+
+    # Input: User enters search query
+    search_query = st.text_input("Enter your search query")
+
+    # Button: User triggers the search
+    if st.button("Search"):
+        if search_query:
+            # Perform the search and get results
+            results = search(search_query)
+
+            # Display search results
+            st.subheader("Search Results")
+            for result in results:
+                with st.container():
+                    if '_source' in result:
+                        try:
+                            st.header(f"{result['_source']['ProductName']}")
+                        except Exception as e:
+                            print(e)
+                        
+                        try:
+                            st.write(f"Description: {result['_source']['Description']}")
+                        except Exception as e:
+                            print(e)
+                        st.divider()
+
+                    
+if __name__ == "__main__":
+    main()
